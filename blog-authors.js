5b737700d52b83937f26ae2756906bcb99b756cf
Regis <mfitumukizaregis90@gmail.com>
2026-04-10 12:00:59 +0200

    blog-authors, posts

---
+++ blog-authors.js
@@ -0,0 +1,11 @@
+(() => {
+  window.BLOG_AUTHORS = {
+    regis: {
+      name: "Regis Mfitumukiza",
+      role: "Full-Stack Developer",
+      avatarText: "MR",
+      avatarFrom: "#22d3ee",
+      avatarTo: "#0ea5e9",
+    },
+  };
+})();
